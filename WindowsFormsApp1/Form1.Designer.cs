c14df163105589962c18cab3c84ba9d2110b5b7a
FGM-DAM1U <fergarmar5@alu.edu.gva.es>
2022-11-24 20:36:49 +0100

    Añadido código "Calculadora telegramas";  añadido archivo solución; se ha resuelto el problema de iniciación del Diseñador.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.cbUrgente = new System.Windows.Forms.CheckBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txtTelegrama = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnResultado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResultado.Location = new System.Drawing.Point(466, 342);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(234, 71);
+            this.btnResultado.TabIndex = 0;
+            this.btnResultado.Text = "Calcular";
+            this.btnResultado.UseVisualStyleBackColor = false;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(36, 32);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(67, 25);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Texto";
+            // 
+            // cbUrgente
+            // 
+            this.cbUrgente.AutoSize = true;
+            this.cbUrgente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbUrgente.Location = new System.Drawing.Point(41, 320);
+            this.cbUrgente.Name = "cbUrgente";
+            this.cbUrgente.Size = new System.Drawing.Size(114, 29);
+            this.cbUrgente.TabIndex = 2;
+            this.cbUrgente.Text = "Urgente?";
+            this.cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(36, 386);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(76, 25);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "Coste:";
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelegrama.Location = new System.Drawing.Point(41, 60);
+            this.txtTelegrama.Multiline = true;
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(659, 229);
+            this.txtTelegrama.TabIndex = 4;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(133, 383);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(203, 30);
+            this.txtPrecio.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(744, 450);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtTelegrama);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.cbUrgente);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnResultado);
+            this.Name = "Form1";
+            this.Text = "Ents.Desarrollo_T2.Práctica3_Calc.PrecioTelegramas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnResultado;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.CheckBox cbUrgente;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txtTelegrama;
+        private System.Windows.Forms.TextBox txtPrecio;
     }
 }
 
